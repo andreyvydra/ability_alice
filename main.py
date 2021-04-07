fffea199b6d5a68a72d10f9a2b68b95c4a69ccba
shmonder-glitch <55046615+shmonder-glitch@users.noreply.github.com>
2021-04-07 22:49:05 +0300

    UPD host and port

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 import logging
 
@@ -88,4 +90,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
