5a776463ae3c2c9f48da763592e78745d128aa24
shmonder-glitch <55046615+shmonder-glitch@users.noreply.github.com>
2021-04-07 23:04:34 +0300

    ADD buying rabbit

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ def handle_dialog(req, res):
                 "Не хочу.",
                 "Не буду.",
                 "Отстань!",
-            ]
+            ],
+            'is_bought_elephant': False
         }
 
         res['response']['text'] = 'Привет! Купи слона!'
@@ -59,14 +60,23 @@ def handle_dialog(req, res):
             'покупаю',
             'хорошо'
         ]:
+            if sessionStorage[user_id]['is_bought_elephant']:
+                res['response']['text'] = 'Кролика можно найти на Яндекс.Маркете!'
+                res['response']['end_session'] = True
+                return
+            else:
+                res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+                sessionStorage[user_id]['is_bought_elephant'] = True
+                return
 
-            res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
-            res['response']['end_session'] = True
-            return
-
-    res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
-    res['response']['buttons'] = get_suggests(user_id)
+    if not sessionStorage[user_id]['is_bought_elephant']:
+        res['response']['text'] = \
+            f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+        res['response']['buttons'] = get_suggests(user_id)
+    else:
+        res['response']['text'] = \
+            f"Все говорят '{req['request']['original_utterance']}', а ты купи кролика!"
+        res['response']['buttons'] = get_suggests(user_id)
 
 
 def get_suggests(user_id):
@@ -80,12 +90,20 @@ def get_suggests(user_id):
     session['suggests'] = session['suggests'][1:]
     sessionStorage[user_id] = session
 
-    if len(suggests) < 2:
-        suggests.append({
-            "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон",
-            "hide": True
-        })
+    if not sessionStorage[user_id]['is_bought_elephant']:
+        if len(suggests) < 2:
+            suggests.append({
+                "title": "Ладно",
+                "url": "https://market.yandex.ru/search?text=слон",
+                "hide": True
+            })
+    else:
+        if len(suggests) < 2:
+            suggests.append({
+                "title": "Ладно",
+                "url": "https://market.yandex.ru/search?text=кролик",
+                "hide": True
+            })
 
     return suggests
 
